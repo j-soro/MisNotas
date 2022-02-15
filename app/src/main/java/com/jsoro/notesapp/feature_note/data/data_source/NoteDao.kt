3a0d33d20edfb9a432dc3729874f94e9fe2bf4a3
Joaquín <74408450+j-soro@users.noreply.github.com>
2022-02-15 18:11:52 +0100

    Establecer las clases relacionadas con la parte de acceso a datos (paquete 'data').
    Por un lado, la entidad que representa una nota, dentro de model (Note.kt), y por otro lado las clases necesarias para el acceso a datos (NoteDao.kt y NoteDatabase.kt).
    
    Ver README para más explicación del funcionamiento del patrón de acceso a datos.

---
+++ app/src/main/java/com/jsoro/notesapp/feature_note/data/data_source/NoteDao.kt
@@ -0,0 +1,21 @@
+package com.jsoro.notesapp.feature_note.data.data_source
+
+import androidx.room.*
+import com.jsoro.notesapp.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+
+    @Query("SELECT * FROM note")
+    fun getNotes(): Flow<List<Note>>
+
+    @Query("SELECT * FROM note WHERe id = :id")
+    suspend fun getNoteById(id: Int): Note?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+}
